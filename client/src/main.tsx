df26b42d3a5e75c3c1a7ca4c20133e3c0697eb3d
2504pratik <21bcs053@iiitdwd.ac.in>
2025-02-06 00:08:06 +0530

    Login Route changes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './sections/LoginPage.tsx';
 
-export const BASE_URL = process.env.BASE_URL || "http://localhost:3000/api"
+export const BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:3000/api";
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router>
