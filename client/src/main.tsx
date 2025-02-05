799efc9278bb3cd657ecdfcf6db8a0e44af782cf
2504pratik <21bcs053@iiitdwd.ac.in>
2025-02-05 23:35:19 +0530

    Changes in BASE_URL

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,7 +5,7 @@ import App from './App.tsx';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './sections/LoginPage.tsx';
 
-export const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api"
+export const BASE_URL = process.env.BASE_URL || "http://localhost:3000/api"
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router>
